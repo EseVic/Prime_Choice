3914d1cbb013c5fc6d39c25e993838ee9ff5dc17
EseVic <esevic111@gmail.com>
2024-09-01 10:42:27 +0100

    fix: updated the settings file

--- primechoice/settings.py
+++ primechoice/settings.py
@@ -14,6 +14,8 @@ SECRET_KEY = '1^_&3^e5&d&^1r)7q21ku7kif0czi07fiw_1^&ref^k!y#_h-d'
 DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+# ALLOWED_HOSTS = []
+
 
 
 # Application definition
@@ -109,8 +111,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [BASE_DIR/'static',]
 STATIC_ROOT = BASE_DIR/'staticfiles'
